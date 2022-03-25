1d742aa6cfc7bd843aae866922fdea7197b1d38f
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-03-25 14:12:08 -0500

    completed menu

--- assets/js/menu/FoodsList.js
+++ assets/js/menu/FoodsList.js
@@ -2,12 +2,50 @@ import * as MenuManger from "./MenuManger.js";
 import { FoodCard } from "./FoodCard.js";
 
 export const FoodList = () => {
-  const contentTarget = document.querySelector("main");
-  let HTMLString;
+  LunchList();
+  DrinkList();
+  AppetizersList();
+  DessertsList();
+};
+
+const LunchList = () => {
   MenuManger.getFoods().then((foodArray) => {
-    HTMLString = `<div class="row">`;
-    HTMLString += foodArray.map((food) => FoodCard(food)).join("");
-    HTMLString += `</div>`;
-    contentTarget.innerHTML = HTMLString;
+    const foundLunchs = foodArray.filter((foodItem) => foodItem.menuId === 1);
+    let html = `<h2>Lunch</h2><div class="row g-0">`;
+    html += foundLunchs.map((lunch) => FoodCard(lunch)).join("");
+    html += `</div>`;
+    document.querySelector("main").innerHTML += html;
+  });
+};
+
+const DrinkList = () => {
+  MenuManger.getFoods().then((foodArray) => {
+    const foundDrinks = foodArray.filter((foodItem) => foodItem.menuId === 2);
+    let html = `<h2>Drinks</h2><div class="row g-0">`;
+    html += foundDrinks.map((drink) => FoodCard(drink)).join("");
+    html += `</div>`;
+    document.querySelector("main").innerHTML += html;
+  });
+};
+
+const AppetizersList = () => {
+  MenuManger.getFoods().then((foodArray) => {
+    const foundAppetizers = foodArray.filter(
+      (foodItem) => foodItem.menuId === 3
+    );
+    let html = `<h2>Appetizers</h2><div class="row g-0">`;
+    html += foundAppetizers.map((appetizer) => FoodCard(appetizer)).join("");
+    html += `</div>`;
+    document.querySelector("main").innerHTML += html;
+  });
+};
+
+const DessertsList = () => {
+  MenuManger.getFoods().then((foodArray) => {
+    const foundDesserts = foodArray.filter((foodItem) => foodItem.menuId === 4);
+    let html = `<h2>Desserts</h2><br><div class="row g-0">`;
+    html += foundDesserts.map((appetizer) => FoodCard(appetizer)).join("");
+    html += `</div>`;
+    document.querySelector("main").innerHTML += html;
   });
 };
