1369dba988cdef19bd9e99cfdf37752f54830d7b
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-03-25 10:48:40 -0500

    pre-merge commit

--- assets/js/menu/FoodsList.js
+++ assets/js/menu/FoodsList.js
@@ -1,15 +1,13 @@
-import * as MenuManger from './MenuManger.js';
-import { FoodCard } from './FoodCard.js'
+import * as MenuManger from "./MenuManger.js";
+import { FoodCard } from "./FoodCard.js";
 
 export const FoodList = () => {
-    const contentTarget = document.querySelector("main")
-    let HTMLString;
-    MenuManger.getFoods()
-    .then(foodArray => {
-        HTMLString = `<div class="row">`
-        HTMLString += foodArray.map(food => FoodCard(food)).join('')
-        HTMLString += `</div>`
-        contentTarget.innerHTML = HTMLString;
-    })
-    
-}
+  const contentTarget = document.querySelector("main");
+  let HTMLString;
+  MenuManger.getFoods().then((foodArray) => {
+    HTMLString = `<div class="row">`;
+    HTMLString += foodArray.map((food) => FoodCard(food)).join("");
+    HTMLString += `</div>`;
+    contentTarget.innerHTML = HTMLString;
+  });
+};
